f091288d29c8e005934c8cd64dc2740f9fb3c6dc
Brad Slagle <btslagle@gmail.com>
2022-05-09 11:42:19 -0500

    Get first 5 test passing

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,18 @@
-export class Triangle {}
+export class Triangle {
+  sideOne = 0;
+  sideTwo = 0;
+  sideThree = 0;
+  constructor(sideOne: number, sideTwo: number, sideThree: number) {
+    this.sideOne = sideOne;
+    this.sideTwo = sideTwo;
+    this.sideThree = sideThree;
+  }
+
+  get isEquilateral(): boolean {
+    return (
+      this.sideOne != 0 &&
+      this.sideOne === this.sideTwo &&
+      this.sideThree === this.sideOne
+    );
+  }
+}
